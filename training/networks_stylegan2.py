629857ca80cfce0fdd2288bf4bba8e46534660cc
daniel-z-kaplan <48258016+daniel-z-kaplan@users.noreply.github.com>
2022-06-28 14:32:48 -0400

    Add files via upload

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -774,7 +774,6 @@ class Discriminator(torch.nn.Module):
         if c_dim > 0:
             self.mapping = MappingNetwork(z_dim=0, c_dim=c_dim, w_dim=cmap_dim, num_ws=None, w_avg_beta=None, **mapping_kwargs)
         self.b4 = DiscriminatorEpilogue(channels_dict[4], cmap_dim=cmap_dim, resolution=4, **epilogue_kwargs, **common_kwargs)
-#         self.latent = DiscriminatorEpilogue(channels_dict[4], cmap_dim=cmap_dim, resolution=4, **epilogue_kwargs, **common_kwargs)
 
     def forward(self, img, c, update_emas=False, **block_kwargs):
         _ = update_emas # unused
@@ -787,7 +786,6 @@ class Discriminator(torch.nn.Module):
         if self.c_dim > 0:
             cmap = self.mapping(None, c)
         x, embedding = self.b4(x, img, cmap)
-#         _, latent = self.latent(x, img, cmap)
         #x is the final logits.
         #
         return x, embedding
